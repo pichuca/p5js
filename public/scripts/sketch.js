96d7ce9f7950cd80b097032c759e8ca4f13560c6
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-06-28 13:18:52 -0300

    Working with NOC random walker.

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,10 +1,44 @@
+/*
+*
+* GLOBALS.
+*
+*/
+
+var walker;
+
 function setup() {
   createCanvas(1240, 700);
+  walker = new Walker();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(random(255));
-    ellipse(mouseX, mouseY, random(80), random(80));
+  // First random drawings! by @sroibon
+  // if (mouseIsPressed) {
+  //   fill(random(255));
+  //   ellipse(mouseX, mouseY, random(80), random(80));
+  // }
+  walker.update();
+  walker.display();
+}
+
+// Daniel Shiffman
+// https://www.kadenze.com/courses/the-nature-of-code
+// http://natureofcode.com/
+// Session 1: Random Walker
+function Walker() {
+
+  // Start walker at center of canvas
+  this.pos = createVector(width / 2, height / 2);
+
+  this.update = function() {
+    var vel = createVector(random(-5, 5), random(-5, 5));
+    this.pos.add(vel);
+  }
+
+  this.display = function() {
+    // draw Walker as circle
+    fill(255);
+    stroke(0);
+    ellipse(this.pos.x, this.pos.y, 48, 48);
   }
 }
