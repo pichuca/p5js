616e73ed8b246a73545e2c851181e42c1d4f83c5
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-06-28 13:19:50 -0300

    Clean commented code.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const app = express();
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 3200;
 
-// app.get('/', (req, res) => res.send('P5 js project!'));
-
 app.use(serveStatic('./public', {'index': ['index.html', 'index.htm']}))
 
 app.listen(port, () => console.log(`P5 js App listening on port ${port}!`));
